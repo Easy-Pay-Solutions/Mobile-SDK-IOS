9c2d48a5d2cb18f495d60fb1d1ec5352f751757a
EasyPay-FPM <easypay-fpm@easypaysoultions.com>
2024-11-12 20:17:58 +0100

    release v1.0.6

--- Sources/EasyPay/Version.swift
+++ Sources/EasyPay/Version.swift
@@ -8,14 +8,14 @@ public enum VersionType {
 }
 
 public class VersionManager {
-    let currentVersion = "1.0.5"
+    let currentVersion = "1.0.6"
     private let minVersion = "1.0.0"
     
     public init() {}
 
     public func checkVersionType() -> VersionType {
         //TODO: Fetch from server
-        let appVersion = "1.0.5"
+        let appVersion = "1.0.6"
         
         if currentVersion == appVersion {
             return .actual
